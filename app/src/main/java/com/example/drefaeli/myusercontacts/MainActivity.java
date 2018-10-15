251ab08430f235635c79040e0af2eeb0a82482e9
David Refaeli <drefaeli@lightricks.com>
2018-10-15 10:49:27 +0300

    feature implemented using list-view, handles permissions at runtime, fix layout and colors

--- app/src/main/java/com/example/drefaeli/myusercontacts/MainActivity.java
+++ app/src/main/java/com/example/drefaeli/myusercontacts/MainActivity.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -17,19 +18,114 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
     private ListView list;
-    private ArrayAdapter<String> adapter;
+    private ArrayAdapter adapter;
     private ArrayList<String> arrayList;
+
+    static final String STATE_LIST = "stateList";
+
+    static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 31000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            arrayList = savedInstanceState.getStringArrayList(STATE_LIST);
+        } else {
+            arrayList = new ArrayList<>();
+        }
+
+        createAdapter();
     }
 
+    private void createAdapter() {
+        list = findViewById(R.id.listview);
+        adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.list_black_text, R.id.list_content, arrayList);
+        list.setAdapter(adapter);
+    }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArrayList(STATE_LIST, arrayList);
+    }
 
+    public void showContacts(View view) {
+        arrayList = new ArrayList<>();
+        createAdapter();
+        if (isPermissionGranted()) {
+            loadContacts();
+        } else {
+            // Permission is not granted
+            requestPermission();
+        }
+    }
+
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_CONTACTS)) {
+            Snackbar snackbar = Snackbar.make(findViewById(R.id.myCoordinatorLayout), "We need your contacts",
+                    Snackbar.LENGTH_INDEFINITE);
+            snackbar.setAction("Ok", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    doRequestPermission();
+                }
+            });
+            snackbar.show();
+        } else {
+            doRequestPermission();
+        }
+    }
+
+    private void doRequestPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_CONTACTS},
+                MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+    }
+
+    private boolean isPermissionGranted() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    loadContacts();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "No permission. Abort", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void loadContacts() {
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null, null, null, null);
+        while (phones.moveToNext()) {
+            String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            arrayList.add(name + " " + phoneNumber);
+            adapter.notifyDataSetChanged();
+        }
+        phones.close();
+    }
 }
-
